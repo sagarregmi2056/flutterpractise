f7e4186de74b6b0aca3c98433b227d7fea56443e
sagar <sagarregmi2056@gmail.com>
2023-08-13 23:54:57 +0545

    my first day

---
+++ lib/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Mylogin extends StatefulWidget {
+  const Mylogin({super.key});
+
+  @override
+  State<Mylogin> createState() => _MyloginState();
+}
+
+class _MyloginState extends State<Mylogin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MaterialApp(home: new Text("hello world"),));
+}
